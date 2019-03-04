a7f201d986a9c5f2168eb02b48219f4adb534a29
Anthony Moss <asmoss93@gmail.com>
2019-03-04 15:08:08 -0500

    adds two functions that print out numbers taking a start and end number, using a for loop and while loop

---
+++ javaScript102.js
@@ -0,0 +1,15 @@
+function printNumbers(start, end) {
+    let counter = start
+    while (counter <= end) {
+        console.log(counter)
+        counter++;
+    }
+}
+printNumbers(1, 10)
+
+function printNumbers2(start, end) {
+    for (let counter = 0; count <= end; counter++) {
+        console.log(counter)
+    }
+}
+printNumbers2(1, 10)
