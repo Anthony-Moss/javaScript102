508de0965b76ff819ff184f0f67c54d3c8fd3242
Anthony Moss <asmoss93@gmail.com>
2019-03-05 11:13:39 -0500

    adds transl33t function that takes a string and outputs it in leetspeak

--- javaScript102.js
+++ javaScript102.js
@@ -115,3 +115,33 @@ function decipher(string, offset) {
 }
 
 console.log(decipher("Gur png  va gur ung", 13))
+
+let leetDictionary = {
+A: 4,
+E: 3,
+G: 6,
+I: 1,
+O: 0,
+S: 5,
+T: 7,
+}
+
+function transl33t(originalString) {
+    let result = '';
+
+    // loop through each letter in origional
+    // if its in the dictionary, and if it is
+    // put its value in `result`
+    for (let i = 0; i < originalString.length; i++) {
+        let theLetter = originalString[i].toUpperCase();
+        console.log(theLetter);
+        console.log(leetDictionary[theLetter]);
+        if (leetDictionary[theLetter] || leetDictionary[theLetter] === 0) {
+            result += leetDictionary[theLetter]
+        } else {
+            result += theLetter
+        }
+    }
+    return result;
+}
+transl33t('hello kitty')
